38ae6d8b2ce49aa352a82976de41554e5543de72
Guruprashanth98 <guruprashanth78@gmail.com>
2021-02-22 12:29:30 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import Order from './pages/Order'
+import Product from './pages/Product'
+import Users from './pages/Users'
+import { BrowserRouter as Router,Route, Switch } from 'react-router-dom';
+import Topbar from './components/Topbar'
+import Login from './pages/Login'
+import ProtectedRoute from './Authentication/ProtectedRoute'
+import LogInRedirect from './Authentication/LogInRedirect'
+import OrderDetail from './pages/OrderDetail';
+import productDetail from './pages/ProductDetail'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Topbar />
+      <Switch>
+        <LogInRedirect path="/" exact Component={Login} />
+        <ProtectedRoute exact path="/order" Component={Order} />
+        <ProtectedRoute exact path="/order/:orderID" Component={OrderDetail} />
+        <ProtectedRoute exact path="/product/:productID" Component={productDetail} />
+        <ProtectedRoute exact path="/product" Component={Product} />
+        <ProtectedRoute exact path="/users" Component={Users} />
+      </Switch> 
+  </Router>
   );
 }
 
